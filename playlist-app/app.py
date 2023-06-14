d0ace9ac56f1fbeb7b3a66b1f4de81a72226b6b8
Casey Wittrock <caseywittrockmn@gmail.com>
2023-06-14 12:43:24 -0500

    update app context, add code for add playlist route

--- playlist-app/app.py
+++ playlist-app/app.py
@@ -1,18 +1,19 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db, Playlist, Song, PlaylistSong
+from models import db, connect_db, Playlist, Song, Playlist_Song
 from forms import NewSongForPlaylistForm, SongForm, PlaylistForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///playlist-app'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///playlist_app"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
-connect_db(app)
-db.create_all()
+with app.app_context():
+    connect_db(app)
+    db.create_all()
 
-app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+app.config["SECRET_KEY"] = "I'LL NEVER TELL!!"
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -26,7 +27,7 @@ debug = DebugToolbarExtension(app)
 def root():
     """Homepage: redirect to /playlists."""
 
-    return redirect("/playlists")
+    return redirect(url_for("show_all_playlists"))
 
 
 ##############################################################################
@@ -38,7 +39,7 @@ def show_all_playlists():
     """Return a list of playlists."""
 
     playlists = Playlist.query.all()
-    return render_template("playlists.html", playlists=playlists)
+    return render_template("/playlists.html", playlists=playlists)
 
 
 @app.route("/playlists/<int:playlist_id>")
@@ -55,8 +56,20 @@ def add_playlist():
     - if form not filled out or invalid: show form
     - if valid: add playlist to SQLA and redirect to list-of-playlists
     """
+    form = PlaylistForm()
 
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+
+        new_playlist = Playlist(name, description)
+
+        db.session.add(new_playlist)
+        db.session.commit()
+
+        return redirect(url_for("show_playlist", playlist_id=new_playlist.id))
+
+    return render_template("/new_playlist.html", form=form)
 
 
 ##############################################################################
@@ -106,11 +119,8 @@ def add_song_to_playlist(playlist_id):
     form.song.choices = ...
 
     if form.validate_on_submit():
+        # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
-          # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+        return redirect(f"/playlists/{playlist_id}")
 
-          return redirect(f"/playlists/{playlist_id}")
-
-    return render_template("add_song_to_playlist.html",
-                             playlist=playlist,
-                             form=form)
+    return render_template("add_song_to_playlist.html", playlist=playlist, form=form)
