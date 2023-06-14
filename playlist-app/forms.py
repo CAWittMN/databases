5a66c61cb40188bfae31a940173fc5215a76dfdd
Casey Wittrock <caseywittrockmn@gmail.com>
2023-06-14 12:44:24 -0500

    create song and playlist forms

--- playlist-app/forms.py
+++ playlist-app/forms.py
@@ -1,23 +1,49 @@
 """Forms for playlist app."""
 
-from wtforms import SelectField
+from wtforms import SelectField, StringField, TextAreaField
 from flask_wtf import FlaskForm
+from wtforms.validators import Length, InputRequired
 
 
 class PlaylistForm(FlaskForm):
     """Form for adding playlists."""
 
-    # Add the necessary code to use this form
+    name = StringField(
+        "Name",
+        validators=[
+            Length(max=20, message="Playlist name is too long"),
+            InputRequired(),
+        ],
+    )
+    description = TextAreaField(
+        "Description",
+        validators=[
+            Length(max=255, message="Playlist description is too long"),
+            InputRequired(),
+        ],
+    )
 
 
 class SongForm(FlaskForm):
     """Form for adding songs."""
 
-    # Add the necessary code to use this form
+    title = StringField(
+        "Title",
+        validators=[
+            Length(max=20, message="Song title is too long"),
+            InputRequired(),
+        ],
+    )
+    artist = StringField(
+        "Artist",
+        validators=[
+            Length(max=20, message="Artist name is too long"),
+            InputRequired(),
+        ],
+    )
 
 
-# DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
 class NewSongForPlaylistForm(FlaskForm):
     """Form for adding a song to playlist."""
 
-    song = SelectField('Song To Add', coerce=int)
+    song = SelectField("Song To Add", coerce=int)
