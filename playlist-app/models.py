5319ed097c6aa3f8e43fb8abe3981cde4087e7d8
Casey Wittrock <caseywittrockmn@gmail.com>
2023-06-14 12:43:58 -0500

    add code for batabase models

--- playlist-app/models.py
+++ playlist-app/models.py
@@ -8,22 +8,46 @@ db = SQLAlchemy()
 class Playlist(db.Model):
     """Playlist."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "playlists"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.String(255), nullable=False)
+
+    songs = db.relationship(
+        "Playlist", secondary="playlists_songs", backref="playlists"
+    )
 
 
 class Song(db.Model):
     """Song."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "songs"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(20), nullable=False)
+    artist = db.Column(db.String(50), nullable=False)
 
 
-class PlaylistSong(db.Model):
+class Playlist_Song(db.Model):
     """Mapping of a playlist to a song."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "playlists_songs"
+
+    playlist_id = db.Column(
+        db.Integer,
+        db.ForeignKey("playlists.id"),
+        primary_key=True,
+        nullable=False,
+    )
+    song_id = db.Column(
+        db.Integer,
+        db.ForeignKey("songs.id"),
+        primary_key=True,
+        nullable=False,
+    )
 
 
-# DO NOT MODIFY THIS FUNCTION
 def connect_db(app):
     """Connect to database."""
 
